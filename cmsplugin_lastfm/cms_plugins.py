1eb4f96ed461585d15980299c8a9508959ea1c56
Chris Glass <christopher.glass@divio.ch>
2010-10-20 14:26:31 +0200

    - First functional version.

--- cmsplugin_lastfm/cms_plugins.py
+++ cmsplugin_lastfm/cms_plugins.py
@@ -4,12 +4,27 @@ from cmsplugin_lastfm import models
 
 import feedparser
 
+from string import Template
+from django.core.cache import cache
+
+# Cache stuff
+CKEY = 'cmsplugin_lastfm_'
+CTIMEOUT = 60*2# 2 minutes
+
+URL = Template('http://ws.audioscrobbler.com/2.0/user/$user/recenttracks.rss')
+
 class RecentTracksPlugin(CMSPluginBase):
     model = models.RecentTracks
     name = 'LastFM Recently Played Tracks Plugin'
     render_template = 'cmsplugin_lastfm/recent_tracks.html'
     
     def render(self, context, instance, placeholder):
-        feed = feedparser.parse({'user':instance.lastfm_user})
+        local_key = "%s%s" % (CKEY, instance.lastfm_user)
+        feed = cache.get(local_key)
+        if not feed:
+            feed = feedparser.parse(URL.substitute({'user':instance.lastfm_user}))
+            cache.set(local_key,feed,CTIMEOUT)
         context.update({'instance': instance, 'feed': feed})
         return context
+    
+plugin_pool.register_plugin(RecentTracksPlugin)
